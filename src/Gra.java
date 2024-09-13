9dbf64a149ad7b602908424e48d01e3d53489e57
yuke-kaynmaster <qutaro008@gmail.com>
2024-09-13 10:30:32 +0200

    essa

--- src/Gra.java
+++ src/Gra.java
@@ -1,2 +1,49 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Gra {
+    private ArrayList<Integer> wylosowane;
+    private LinkedList<Integer> wpisane;
+    private ArrayList<Integer> trafione;
+
+    private void wylosujLiczby(int ile){
+        wylosowane = new ArrayList<>();
+        int liczba;
+        for (int i = 0; i < ile; i++) {
+            liczba = (int)(Math.random()*100+1);
+            while (wylosowane.contains(liczba)){
+                liczba = (int)(Math.random()*100+1);
+            }
+            wylosowane.add(liczba);
+        }
+    }
+
+    private void wczytajLiczby(int ile){
+        wpisane = new LinkedList<>();
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("podaj "+ile+" liczba");
+        for (int i = 0; i < ile; i++) {
+            wpisane.add(klawiatura.nextInt());
+        }
+    }
+
+    private void wypiszListe(List<Integer> lista){
+        for (Integer element:lista){
+            System.out.println(element+", ");
+        }
+    }
+
+    private void sprawdzKtorePowtarzajaSie(ArrayList<Integer> wpisane,LinkedList<Integer> wylosowane){
+        trafione = new ArrayList<>();
+        for (Integer element:wylosowane){
+            if(wpisane.contains(element)){
+                trafione.add(element);
+            }
+        }
+    }
+
+
+
 }
